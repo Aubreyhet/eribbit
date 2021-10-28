5ec36cc097fe8969aeea474aca75c45fa9d38cde
Aubreyhet <2930603106@qq.com>
2021-10-29 01:15:11 +0800

    首页广告开发完成

--- src/api/home.js
+++ src/api/home.js
@@ -1,10 +1,26 @@
 // 定义首页需要的数据接口
 import resquest from '@/utils/request'
 
+/**
+ * 首页获取分类数据接口
+ * @param {*} params limit
+ * @returns 返回数据对象
+ */
 export const findBrand = params => {
   return resquest({
     method: 'GET',
     url: '/home/brand',
     params
+  })
+}
+
+/**
+ * 获取首页轮播图数据
+ * @returns 数据对象
+ */
+export const findBanner = () => {
+  return resquest({
+    method: 'GET',
+    url: '/home/banner'
   })
 }
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -5,16 +5,19 @@
       <!-- 左侧分类 -->
       <HomeCategory />
       <!-- 轮播图 -->
+      <HomeBanner />
     </div>
   </div>
 </template>
 
 <script>
+import HomeBanner from './components/home-banner.vue'
 import HomeCategory from './components/home-category.vue'
 export default {
   name: 'HomePage',
   components: {
-    HomeCategory
+    HomeCategory,
+    HomeBanner
   }
 }
 </script>
